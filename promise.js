be59b34b18274add27453ba4676a7909a42bdeeb
jola_adebayo <adebayoanjola@gmail.com>
2020-03-12 14:25:22 +0100

    completed question one

---
+++ promise.js
@@ -0,0 +1,54 @@
+/**
+ first function
+
+ */
+/**
+ * Concatenates the supplied string to another to return a full name
+ *
+ * @param {String } firstName   The string to create a fullname
+ * @param { callback} callback The action to perform
+ */
+
+
+const f = (firstName, callback) => {
+  setTimeout(() => {
+    if (!firstName) return callback(new Error('firstName is required'))
+    const fullName = `${firstName} Smith`
+    return callback(fullName)
+  }, 2000)
+}
+
+f('Andrew', console.log)
+f( null, console.log )
+
+ 
+
+/**
+ * Concatenates the supplied string to another to return a full name
+ *
+ * @param {String } firstName   The string to create a fullname
+ * @param { callback} callback The action to perform
+ */
+
+
+const Newfn = ( firstName ) => {
+    return new Promise( ( resolve, reject ) => {
+
+        setTimeout( () => {
+            if ( !firstName ) return reject( new Error( 'firstName is required' ) )
+            const fullName = `${ firstName } Smith`
+            return resolve( fullName )
+
+        }, 2000 )
+    } )
+
+}
+Newfn( 'Anjolaoluwa' )
+    .then( data => console.log(data)  )
+    .catch( e => console.log( e,  'error' ) )
+    .finally( console.log('finally, promise resolved') )
+
+Newfn( null )
+    .then( data => console.log( data ) )
+    .catch( e => console.log( e, 'error' ) )
+    .finally( console.log( 'finally, promise resolved' ) )
